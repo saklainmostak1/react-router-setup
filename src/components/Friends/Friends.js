23ca4b952235371dbe3553e340876ae84004d9f3
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-03 04:01:27 +0600

    friend

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const friends = useLoaderData();
+
     return (
         <div>
-            <h2>I have so many friend</h2>
+            <h2>I have so many friend: {friends.length} </h2>
+           {
+            friends.map(friend => <Friend
+            key={friend.id}
+            friend={friend}
+            ></Friend>)
+           } 
         </div>
     );
 };
