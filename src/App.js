5def3ef748e27c6c4097d61e4d1a5a17e43c14c5
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-03 02:50:18 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
 
 function App() {
   const router = createBrowserRouter([
    {path: '/', element: <div>This is Deafult page</div>},
-    { path: '/home', element: <div>Home Page</div>},
-    { path:'about', element: <div>This is about page</div>}
+    { path: '/home', element: <Home></Home>},
+    { path:'about', element: <About></About>},
+    { path:'products', element: <Products></Products>},
   ])
   return (
     <div className="App">
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+
+const Home = () => {
+    return (
+        <div>
+            
+            <Header></Header>
+            <h3>This is real home components</h3>
+        </div>
+    );
+};
+
+export default Home;
