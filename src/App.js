00b25c18f65990861430696d39513874d389e0f8
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-03 05:28:31 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
+import FriendDetails from './components/FriendDetails/FriendDetails';
 import Friends from './components/Friends/Friends';
 import Home from './components/Home/Home';
+import Post from './components/Posts/Posts';
 import Products from './components/Products/Products';
 import Main from './Layout/Main';
 
@@ -17,7 +19,23 @@ function App() {
       loader: async () =>{
         return fetch('https://jsonplaceholder.typicode.com/users')
       },
-      element: <Friends></Friends>},
+      element: <Friends></Friends>
+    },
+    {
+      path:'/friend/:friendId',
+      loader: async({params}) =>{
+        // console.log(params.freiendId)
+        return fetch(`https://jsonplaceholder.typicode.com/users/${params.friendId}`)
+      },
+      element: <FriendDetails></FriendDetails>
+    },
+    {
+      path: 'posts',
+      loader: async() =>{
+        return fetch('https://jsonplaceholder.typicode.com/users')
+      },
+      element: <Post></Post>
+    }
    ]},
    
     { path:'about', element: <About></About>},
